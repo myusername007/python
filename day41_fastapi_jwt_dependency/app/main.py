23ad5208ccaf678d5f97baaccf5076373ceda9da
myusername007 <nytka.roman@student.uzhnu.edu.ua>
2026-01-24 22:12:40 +0200

    Alembic migrations basics study

--- day41_fastapi_jwt_dependency/app/main.py
+++ day41_fastapi_jwt_dependency/app/main.py
@@ -3,8 +3,6 @@ from app.db.base import Base
 from app.db.session import engine
 from app.routers import users, auth, posts
 
-Base.metadata.create_all(bind=engine)
-
 app = FastAPI()
 
 app.include_router(users.router, prefix="/users", tags=["users"])
